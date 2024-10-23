8f80dd386ed5f10b70c4757848653f28636e8549
kyukyu0909 <miitan.dance@gmail.com>
2024-10-24 02:48:19 +0900

    会員側の会員管理機能

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,11 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin;
 use App\Http\Controllers\Admin\RestaurantController;
-use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\CompanyController;
 use App\Http\Controllers\Admin\TermController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,28 +21,34 @@ use App\Http\Controllers\Admin\TermController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
-Route::get('/user', [UserController::class, 'index']);
+//Route::get('/user', [UserController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+//Route::middleware('auth')->group(function () {
+    //Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    //Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    //Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//});
 
-Route::middleware('auth:admin')->group(function () {
-    Route::get('admin/users', [App\Http\Controllers\Admin\UserController::class, 'index'])->name('admin.users.index');
-    Route::get('admin/users/{user}', [App\Http\Controllers\Admin\UserController::class, 'show'])->name('admin.users.show');
+//Route::middleware('auth:admin')->group(function () {
+    //Route::get('admin/users', [App\Http\Controllers\Admin\UserController::class, 'index'])->name('admin.users.index');
+    //Route::get('admin/users/{user}', [App\Http\Controllers\Admin\UserController::class, 'show'])->name('admin.users.show');
+//});
+
+Route::group(['middleware' => 'guest:admin'], function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::resource('user', UserController::class)->only(['index', 'edit', 'update'])->middleware(['auth', 'verified'])->names('user');
 });
 
 require __DIR__.'/auth.php';
+
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth:admin'], function () {
     Route::get('home', [Admin\HomeController::class, 'index'])->name('home');
@@ -66,10 +73,23 @@ Route::resource('admin/categories', CategoryController::class)->only(['index', '
 
 Route::prefix('admin/company')->group(function () {
     Route::get('/index', [CompanyController::class, 'index'])->name('admin.company.index');
-    Route::get('/edit/{company}', [CompanyController::class, 'edit'])->name('admin.company.edit');
-    Route::patch('/edit/{company}', [CompanyController::class, 'update'])->name('admin.company.update');
+    Route::get('/edit', [CompanyController::class, 'edit'])->name('admin.company.edit');
+    Route::patch('/edit', [CompanyController::class, 'update'])->name('admin.company.update');
 });
 
-Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth:admin'], function () {
-    Route::resource('terms', TermController::class);
+Route::prefix('admin/terms')->group(function () {
+    Route::get('/index', [TermController::class, 'index'])->name('admin.terms.index');
+    Route::get('/edit', [TermController::class, 'edit'])->name('admin.terms.edit');
+    Route::patch('/edit', [TermController::class, 'update'])->name('admin.terms.update');
 });
+
+
+//Route::prefix('admin/company')->group(function () {
+    //Route::get('/index', [CompanyController::class, 'index'])->name('admin.company.index');
+    //Route::get('/edit/{company}', [CompanyController::class, 'edit'])->name('admin.company.edit');
+    //Route::patch('/edit/{company}', [CompanyController::class, 'update'])->name('admin.company.update');
+//});
+
+//Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth:admin'], function () {
+    //Route::resource('terms', TermController::class);
+//});
