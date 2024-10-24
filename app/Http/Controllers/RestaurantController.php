b880d471dae262105ead0a0d4a11ac88ee1c3e72
kyukyu0909 <miitan.dance@gmail.com>
2024-10-24 23:44:41 +0900

    会員側の店舗一覧ページ

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -10,9 +10,9 @@ class RestaurantController extends Controller
 {
     public function index(Request $request)
     {
-        $keyword = $request->keyword;
-        $category_id = $request->category_id;
-        $price = $request->price;
+        $keyword = $request->input('keyword');
+        $category_id = $request->input('category_id');
+        $price = $request->input('price');
 
         $sorts = [
             '掲載日が新しい順' => 'created_at desc',
@@ -20,44 +20,42 @@ class RestaurantController extends Controller
         ];
 
         $sort_query = [];
-    if ($request->has('select_sort')) {
-        $slices = explode(' ', $request->input('select_sort'));
-        $sort_query[$slices[0]] = $slices[1];
-    } else {
-        $sort_query = ['created_at' => 'desc'];
-    }
 
-    $categories = Category::all();
+        $sorted = 'created_at desc';
 
-    $restaurants = Restaurant::query();
-
-    if ($keyword) {
-        $restaurants->where('name', 'like', "%{$keyword}%")
-            ->orWhere('address', 'like', "%{$keyword}%")
-            ->orWhereHas('categories', function ($query) use ($keyword) {
-                $query->where('categories.name', 'like', "%{$keyword}%");
-            });
-    }
-
-    if ($category_id) {
-        $restaurants->whereHas('categories', function ($query) use ($category_id) {
-            $query->where('categories.id', '=', $category_id);
-        });
-    }
-
-    if ($price) {
-        $priceRange = explode('-', $price);
-        if (count($priceRange) == 2) {
-            $restaurants->whereBetween('lowest_price', [$priceRange[0], $priceRange[1]]);
-        } else {
-            $restaurants->where('lowest_price', '<=', $price);
+        if ($request->has('select_sort')) {
+            $slices = explode(' ', $request->input('select_sort'));
+            $sort_query[$slices[0]] = $slices[1];
+            $sort_query = ['created_at' => 'desc'];
         }
+
+        $categories = Category::all();
+
+        $restaurants = Restaurant::query();
+
+        if ($keyword) {
+            $restaurants = Restaurant::where('name', 'like', "%{$keyword}%")
+                ->orWhere('address', 'like', "%{$keyword}%")
+                ->orWhereHas('categories', function ($query) use ($keyword) {
+                    $query->where('categories.name', 'like', "%{$keyword}%");
+                })
+                ->sortable($sort_query)
+                ->orderBy('created_at', 'desc')
+                ->paginate(15);
+        } elseif ($category_id) {
+            $restaurants = Restaurant::whereHas('categories', function ($query) use ($category_id) {
+                $query->where('categories.id', $category_id);
+            })->sortable($sort_query)->orderBy('created_at', 'desc')->paginate(15);
+        } elseif ($price) {
+            $restaurants = Restaurant::where('lowest_price', '<=', $price)->sortable($sort_query)->orderBy('created_at', 'desc')->paginate(15);
+        } else {
+            $restaurants = Restaurant::sortable($sort_query)->orderBy('created_at', 'desc')->paginate(15);
+        }
+
+        $categories = Category::all();
+
+        $total = $restaurants->total();
+
+        return view('restaurants.index', compact('keyword', 'category_id', 'price', 'sorts', 'sorted', 'restaurants', 'categories', 'total'));
     }
-
-    $restaurants = $restaurants->sortable($sort_query)->paginate(15);
-
-    $total = $restaurants->total();
-
-    return view('restaurants.index', compact('keyword', 'category_id', 'price', 'sorts', 'sorted', 'restaurants', 'categories', 'total'));
-    }   
 }
--- resources/views/restaurants/index.blade.php
+++ resources/views/restaurants/index.blade.php
@@ -108,8 +108,8 @@
                             <div class="card h-100">
                                 <div class="row g-0">
                                     <div class="col-md-4">
-                                        @if ($restaurant->image !== '')
-                                            <img src="{{ asset('storage/restaurants/' . $restaurant->image) }}" class="card-img-top nagoyameshi-horizontal-card-image">
+                                        @if ($restaurant->image_name !== '')
+                                            <img src="{{ asset('storage/restaurants/' . $restaurant->image_name) }}" class="card-img-top nagoyameshi-horizontal-card-image">
                                         @else
                                             <img src="{{ asset('/images/no_image.jpg') }}" class="card-img-top nagoyameshi-horizontal-card-image" alt="画像なし">
                                         @endif
