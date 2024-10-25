d547e42198e11db28855b6f67a40d0a7943e9c90
kyukyu0909 <miitan.dance@gmail.com>
2024-10-25 13:52:36 +0900

    修正

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,8 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
-    public const ADMIN_HOME = '/admin/home';
+    public const HOME = '/';
+    public const ADMIN_HOME = 'admin/home';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
